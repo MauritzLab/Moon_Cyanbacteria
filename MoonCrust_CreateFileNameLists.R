dbcc9f3e2bbea062c5bf436019877bdc3bb247e5
MauritzLab <memauritz@utep.edu>
2023-03-07 17:42:24 -0700

    updated to improve file info and add code to save

--- MoonCrust_CreateFileNameLists.R
+++ MoonCrust_CreateFileNameLists.R
@@ -4,8 +4,6 @@
 # written: M. Mauritz 7 Mar 2023
 
 
-setwd("G:/.shortcut-targets-by-id/1UPSTmVsOZCuZ8FOMhJhIO-1E6uzij3to/Moon_Cyanobacteria")
-
 moon_all <- list.files("G:/.shortcut-targets-by-id/1UPSTmVsOZCuZ8FOMhJhIO-1E6uzij3to/Moon_Cyanobacteria")
  
 moon1 <-  list.files(paste("G:/.shortcut-targets-by-id/1UPSTmVsOZCuZ8FOMhJhIO-1E6uzij3to/Moon_Cyanobacteria",
@@ -15,4 +13,12 @@ moon2 <-  list.files(paste("G:/.shortcut-targets-by-id/1UPSTmVsOZCuZ8FOMhJhIO-1E
                            moon_all[2],sep="/"), pattern="IMG")
 
 # turn the list into a dataframe that can be saved
-moon1 <- data.frame(filename=moon1, folder=moon_all[1])
+moon1 <- data.frame(folder=moon_all[1],
+                    date=strsplit(moon_all[1],"_")[[1]][1],
+                    filename=moon1)
+
+# Save back to GDrive
+setwd("G:/.shortcut-targets-by-id/1UPSTmVsOZCuZ8FOMhJhIO-1E6uzij3to/Moon_Cyanobacteria/Phenoanalyser_Files")
+
+write.csv(moon1,row.names=FALSE,
+          file=paste("FileInfo_",(strsplit(moon_all[1],"_")[[1]][1]),".csv",sep=""))
